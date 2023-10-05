dec51aeeba518d3b05cfee0c3e40cca8c713cd6f
Eddie Webb <ollitech@gmail.com>
2023-10-05 14:11:51 -0400

    script dont erorr on 503

--- change_nexus_password.sh
+++ change_nexus_password.sh
@@ -1,7 +1,17 @@
 NEW_PASSWORD=${1}
 REGION=${2}
 
-curl -ifu admin:"admin123" \
+
+
+response=$(curl -w "%{http_code}"  -ifu admin:"admin123" \
       -XPUT -H 'Content-Type: text/plain' \
       --data "${NEW_PASSWORD}" \
-      https://nexus.${REGION}.circleci-labs.com/service/rest/v1/security/users/admin/change-password
+      https://nexus.${REGION}.circleci-labs.com/service/rest/v1/security/users/admin/change-password \
+      > response.txt)
+
+
+if [ "$response" == "503" ]; then
+      #Nexus not there yet, so we just silently proceed.
+else
+      #nexus admin set, celebrate
+fi
