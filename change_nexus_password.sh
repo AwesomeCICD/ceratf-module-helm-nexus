9a5006971b9680cb37083a3ba189ff54ec0b5862
Eddie Webb <ollitech@gmail.com>
2024-06-22 10:37:43 -0400

    increase delay

--- change_nexus_password.sh
+++ change_nexus_password.sh
@@ -2,18 +2,38 @@ NEW_PASSWORD=${1}
 TARGET_DOMAIN=${2}
 set +e
 
+echo "This should only run when nexus first created, and JVMs dont start quickly. Curl will retry for 3 minutes."
 
-response=$(curl -w "%{http_code}"  -iu admin:"admin123" \
-      -XPUT -H 'Content-Type: text/plain' \
-      --data "${NEW_PASSWORD}" \
-      https://nexus.${TARGET_DOMAIN}/service/rest/v1/security/users/admin/change-password \
-      -o response.txt 2>/dev/null)
+let attempts=0
+
+function post_creds {
+
+      response=$(curl -w "%{http_code}"  -iu admin:"admin123" \
+            -XPUT -H 'Content-Type: text/plain' \
+            --data "${NEW_PASSWORD}" \
+            --connect-timeout 2 \
+            --max-time 5 \
+            --retry 30 \
+            --retry-delay 5 \
+            --retry-max-time 180 \
+            https://nexus.${TARGET_DOMAIN}/service/rest/v1/security/users/admin/change-password \
+            -o response.txt 2>/dev/null)
 
 
-if [ "$response" == "503" ]; then
-      #Nexus not there yet, so we just silently proceed.
-      echo "Nothing to do yet"
-else
-      #nexus admin set, celebrate
-      echo "job well done"
-fi
+      if [ "$response" == "503" ]; then
+      #       if [ $attempts -gt 4 ]; then 
+      #             echo "Gave up after $attempts attempts"
+      #             exit 0
+      #       fi
+            echo "503 in right cycle means we're waiting to start, but may be encountered in other lifecycles. "
+            echo "We have waited to start and now give up, assuming we're in one of those intentional corner cases."
+           # sleep 30
+            #let attempts=attempts+1
+            #post_creds
+      else
+            #nexus admin set, celebrate
+            echo "job well done"
+      fi
+}
+
+post_creds
